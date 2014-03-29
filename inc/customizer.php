237935169fee7e6744ef0dbd0956fb6d7db79244
ishihara_takashi <ishihara_note02@ishihara-note02.local>
2014-03-29 22:06:06 +0900

    first commit.

---
+++ inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * epigone Theme Customizer
+ *
+ * @package epigone
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function epigone_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'epigone_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function epigone_customize_preview_js() {
+	wp_enqueue_script( 'epigone_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20130508', true );
+}
+add_action( 'customize_preview_init', 'epigone_customize_preview_js' );
