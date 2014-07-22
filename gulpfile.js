046020d6c89521a92e2091ddf6975c7f666caa35
ishihara_takashi <ishihara_note02@ishihara-note02.local>
2014-07-22 23:14:49 +0900

    Fixed proxy domain

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ gulp.task('browserSync', function() {
   browserSync.init( null, {
     notify: true,
     proxy: {
-      host: "grow-group.jp", // replace your domain
+      host: "your-domain.dev", // replace your domain
       //port: 3000
     },
     ghostMode: {
